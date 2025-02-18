6d4a4568a8d6cc39d6f473b75c2d30d9ed8400ff
Joel Lehtonen <jlehtone@c1r6p6.hive.fi>
2025-02-18 08:21:17 +0200

    reformated test for ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,63 +1,40 @@
 #include "Bureaucrat.hpp"
 
 int main(void) {
+
+    std::cout << "\n---TEST #1: Creating Bureaucrats---\n";
     try {
+        std::cout << "Creating Alice\n";
         Bureaucrat Alice("Alice", 2);
-    }
-    catch (Bureaucrat::GradeTooHighException& e)
-    {
-        std::cout << "Creation exception occured: " << e.what() << std::endl;
-    }
-    catch (Bureaucrat::GradeTooLowException& e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Creation exception occured: " << e.what() << std::endl;
     }
     try {
+        std::cout << "Creating Bob\n";
         Bureaucrat Bob("Bob", 200); //will throw exception
-    }
-    catch (Bureaucrat::GradeTooHighException& e)
-    {
-        std::cout << "Creation exception occured: " << e.what() << std::endl;
-    }
-    catch (Bureaucrat::GradeTooLowException& e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Creation exception occured: " << e.what() << std::endl;
     }
     try {
+        std::cout << "Creating Charlie\n";
         Bureaucrat Charlie("Charlie", 0); //will throw exception
-    }
-    catch (Bureaucrat::GradeTooHighException& e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Creation exception occured: " << e.what() << std::endl;
     }
-    catch (Bureaucrat::GradeTooLowException& e)
-    {
-        std::cout << "Creation exception occured: " << e.what() << std::endl;
-    }
+
+    std::cout << "\n---TEST #2: Incremeting and Decrementing---\n";
     Bureaucrat Charlie("Charlie", 2);
     Bureaucrat David("David", 1);
     try {
         Charlie.incrementGrade();
         David.incrementGrade(); //will throw exception
-    }
-    catch (Bureaucrat::GradeTooHighException& e)
-    {
-        std::cout << "Incrementing exception occured: " << e.what() << std::endl;
-    }
-    catch (Bureaucrat::GradeTooLowException& e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Incrementing exception occured: " << e.what() << std::endl;
     }
     try {
         for (int i = 0; i < 10; i++)
             Charlie.decrementGrade();
-    }
-    catch (Bureaucrat::GradeTooHighException& e)
-    {
-        std::cout << "Decrementing exception occured: " << e.what() << std::endl;
-    }
-    catch (Bureaucrat::GradeTooLowException& e)
-    {
+    } catch (std::exception& e) {
         std::cout << "Decrementing exception occured: " << e.what() << std::endl;
     }
     std::cout << "Here is " << Charlie << " and " << David << std::endl;
